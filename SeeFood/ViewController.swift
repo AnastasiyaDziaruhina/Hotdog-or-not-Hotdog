bb01ea5b8d0b281bc8781b3f572c930c76de8f2e
stacysfish <st.vitalievna@yandex.ru>
2022-07-25 21:03:57 +0200

    Getting image recognition back to model

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         
         imagePicker.delegate = self
-        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = true
         
     }
@@ -28,12 +28,47 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+           
             self.imageView.image = userPickedImage
+           
+           guard let ciimage = CIImage(image: userPickedImage) else {
+               fatalError("Could not convert to CIImage")
+           }
+           
+           detect(image: ciimage)
+           
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
     }
 
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else {
+            fatalError("Loading CoreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try! handler.perform([request])
+            
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
         present(imagePicker, animated: true, completion: nil)
